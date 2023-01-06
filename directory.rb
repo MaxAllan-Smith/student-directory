e524e4eb46307dd85a52fda75b04f7a9082f767a
Max Allan-Smith <Maxibon_1195@hotmail.com>
2023-01-06 10:47:47 +0000

    Update to student directory

--- directory.rb
+++ directory.rb
@@ -64,10 +64,22 @@ def process(selection)
   end
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def interactive_menu
   loop do
     print_menu
     process(gets.chomp)
+    save_students
   end
 end
 
