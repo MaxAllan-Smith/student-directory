ee9d8a9da432ce7036bf25aab15ae16787b9a3a9
Max Allan-Smith <Maxibon_1195@hotmail.com>
2023-01-06 11:01:43 +0000

    Updated Directory

--- directory.rb
+++ directory.rb
@@ -43,6 +43,7 @@ def print_menu
   puts "1: Input the students"
   puts "2: Show the students"
   puts "3: Save student to file"
+  puts "4: Load the list from students.csv"
   puts "9: Exit"
 end
 
@@ -60,6 +61,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -78,6 +81,27 @@ def save_students
   file.close
 
   puts "Students saved to file 'students.csv' successfully"
+end
+
+def load_students( filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+      @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exist?(filename) # if it exists
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
 end
 
 def interactive_menu
