b2d9f556a59a2939706289438754b5a3a9e3ae03
Max Allan-Smith <Maxibon_1195@hotmail.com>
2023-01-06 10:53:59 +0000

    added 'save_students' to process. Created .gitignore and added students.csv to .gitignore

--- directory.rb
+++ directory.rb
@@ -42,6 +42,7 @@ end
 def print_menu
   puts "1: Input the students"
   puts "2: Show the students"
+  puts "3: Save student to file"
   puts "9: Exit"
 end
 
@@ -57,6 +58,8 @@ def process(selection)
     @students = input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -73,6 +76,8 @@ def save_students
     file.puts csv_line
   end
   file.close
+
+  puts "Students saved to file 'students.csv' successfully"
 end
 
 def interactive_menu
